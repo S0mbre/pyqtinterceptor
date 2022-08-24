13a09e61a8463542217db89eaffeb87c1b7f8b1c
S0mbre <s00mbre@gmail.com>
2022-08-24 14:40:22 +1100

    buildexe

--- buildexe.bat
+++ buildexe.bat
@@ -1,4 +1,3 @@
 @echo off
-python -m pyinstaller -w --name=interceptor --add-data="config.ini;." --noconfirm main.py
-7z a -r -y ./dist/interceptor.7z ./dist/interceptor/*.*
+pyinstaller -w --name=interceptor --add-data="config.ini;." --noconfirm main.py && 7z a -r -y ./dist/interceptor.7z ./dist/interceptor/*.*
 pause > nul
